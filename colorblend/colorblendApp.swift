338fd160611a360171a529238402e564bd04fddc
Fajar Wirazdi <fajarwirazdi@gmailo.com>
2023-07-07 15:19:47 +0700

    Initial Commit

---
+++ colorblend/colorblendApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorblendApp.swift
+//  colorblend
+//
+//  Created by Fajar Wirazdi on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct colorblendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

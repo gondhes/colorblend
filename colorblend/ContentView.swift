74a49cd217bb9c3c14cfd5d6c21c4ef44d9cdeb8
Adinda Dwi <adindadwi027@gmail.com>
2023-07-07 16:59:03 +0700

    ganti nama jadi dinda

--- colorblend/ContentView.swift
+++ colorblend/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, color!")
+            Text("Hello, dinda!")
         }
         .padding()
     }
